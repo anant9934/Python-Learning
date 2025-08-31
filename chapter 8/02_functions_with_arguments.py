6ed7bfbbb7f8c661a98ff6e60e6ee0b6f9a22c07
ANANT KUMAR <720anant@gmail.com>
2025-08-31 17:49:02 +0530

    upadted code

--- chapter 8/02_functions_with_arguments.py
+++ chapter 8/02_functions_with_arguments.py
@@ -1,4 +1,4 @@
-def arg(name,endeing):
-    print
+def arg(name, ending):      
+    print(f"{name}, {ending}")
 arg("anant", "good morning!")  # function call
-arg("John", "See you soon!")  # another function call
+arg("John", "See you soon!")   # another function call
